487e24461c8fbe70932eadaf719221a51ca8f30a
Mégane Schaetzel <megane.schaetzel@ccicampus.fr>
2026-03-25 16:23:06 +0100

    exercice

---
+++ src/Twig/Components/BootstrapButton.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class BootstrapButton
+{
+    public string $text;
+    public string $type;
+    public string $link;
+
+}
