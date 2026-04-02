2593060a5c27404b63b770aef31629381ee65074
Mégane Schaetzel <megane.schaetzel@ccicampus.fr>
2026-04-02 16:24:35 +0200

    clé étrangère

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -11,9 +11,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/pokemon', name: 'app_pokemon_')]
+
 final class PokemonController extends AbstractController
 {
-    #[Route('/pokemon', name: 'app_pokemon')]
+    #[Route('/', name: 'index')]
     public function index(PokemonRepository $pokemonRepository): Response
     {
         $arrPokemon = $pokemonRepository->findAll();
@@ -24,7 +26,7 @@ final class PokemonController extends AbstractController
     }
 
 
-    #[Route('/pokemon/create', name: 'app_pokemon_create')]
+    #[Route('/create', name: 'create')]
     public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         /*
@@ -69,13 +71,32 @@ final class PokemonController extends AbstractController
 
         $createForm = $this->createForm(PokemonCreateFormType::class, $objNewPokemon);
 
-        return $this->render('pokemon/create.html.twig', [
+
+        // J'envoi les données de la requête au formulaire
+        $createForm->handleRequest($request);
+
+
+        // Vérifie si le formulaire est soumis et que les données sont valides
+        if ($createForm->isSubmitted() && $createForm->isValid()) {
+
+            $entityManager->persist($objNewPokemon);
+            $entityManager->flush();
+
+            $this->addFlash('success', "Le pokémon a bien été crée en base");
+
+            return $this->redirectToRoute('app_pokemon_show', [
+                'id' => $objNewPokemon->getId()
+            ]);
+        }
+
+
+        return $this->render('pokemon/form.html.twig', [
             'createForm'    => $createForm
         ]);
     }
 
     //c'est une expression régulière qui permet de valider mon paramètre : obligé que ce soit un nombre -> \d un chiffre et le + : au moins 1
-    #[Route('/pokemon/{id<\d+>}', name: 'app_pokemon_show')]
+    #[Route('/{id<\d+>}', name: 'show')]
     public function show(Pokemon $pokemon): Response
     {
         /*
@@ -96,6 +117,35 @@ final class PokemonController extends AbstractController
             'pokemon'    => $objPokemon
             */
             'pokemon' => $pokemon
+        ]);
+    }
+
+    #[Route('/{id<\d+>}/update', name: 'update')] //< URL : /pokemon/1/update
+    public function update(Pokemon $pokemon, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // On construit le formulaire à partir des données de l'entité récupérée
+        // depuis l'ID présent dans l'URL
+        $updateForm = $this->createForm(PokemonCreateFormType::class, $pokemon);
+
+        $updateForm->handleRequest($request);
+
+        if($updateForm->isSubmitted() && $updateForm->isValid()) {
+
+            // L'entité provenant déjà de la base, Doctrine la connait
+            // => pas besoin de persist
+
+            $entityManager->flush();
+
+            $this->addFlash('success', "Le pokémon a bien été modifié en base");
+
+            // Redirige vers la page de détails du pokémon modifié
+            return $this->redirectToRoute('app_pokemon_show', [
+                'id' => $pokemon->getId()
+            ]);
+        }
+
+        return $this->render('pokemon/form.html.twig', [
+            'createForm'    => $updateForm
         ]);
     }
 }
--- src/Twig/Components/BootstrapCard.php
+++ src/Twig/Components/BootstrapCard.php
@@ -7,54 +7,39 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class BootstrapCard
 {
-
     private string $_strTitle;
     private string $_strImg;
-    private string $_strLink;
-    private string $_strLabel;
-
-
-    public function mount(string $title, string $img, string $link, string $label) {
-
-        $this->_strTitle = $title;
-        $this->_strImg   = $img;
-        $this->_strLink  = $link;
-        $this->_strLabel = $label;
-        
-    }
+    private array $_arrActions;
 
     /**
-    * 
-    */
-    public function getTitle(): string 
+     * Monte le composant dans le DOM
+     * 
+     * @param string $title Titre de la card
+     * @param string $img l'URL ou l'URI de la source de l'image
+     * @param string $link l'URL ou l'URI du target du lien du bouton
+     * @param string $label Texte affiché dans le bouton
+     */
+    public function mount(string $title, string $img, array $actions = []): void
+    {
+        $this->_strTitle    = $title;
+        $this->_strImg      = $img;
+        $this->_arrActions  = $actions;
+    }
+
+    // GETTERS ================================================================
+
+    public function getTitle(): string
     {
         return $this->_strTitle;
     }
 
-    /**
-    * 
-    */
-    public function getImg(): string 
+    public function getImg(): string
     {
         return $this->_strImg;
     }
 
-     /**
-    * 
-    */
-    public function getLink(): string 
+    public function getActions(): array
     {
-        return $this->_strLink;
+        return $this->_arrActions;
     }
-
-     /**
-    * 
-    */
-    public function getLabel(): string 
-    {
-        return $this->_strLabel;
-    }
-
-
-
 }
